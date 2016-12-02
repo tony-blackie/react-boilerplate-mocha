4e24bd2c6d68e09a2706c3b4ef954c7e19db5e88
Anton Spaskiy <anton.spaskiy@dataart.com>
2016-12-02 16:16:06 +0100

    Add server logic

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+const PORT = 3000;
+const usersPath = path.join(__dirname + '/app/fixtures/data/users.json');
+const users = require(usersPath);
+
+app.use(express.static(__dirname + '/app/templates/index.html'));
+
 app.get('/users', (req, res) => {
-    res.send(path.join(__dirname + '/app/fixtures/data/users.json'));
+    console.log(`Serving json with ${users}`);
+    // res.send(users);
+    res.send('Hello World');
 });
+
+console.log(`Server is listening on port ${PORT}`);
+app.listen(PORT);
